6462500cbc71e19241afd5bb14b837433d065176
augusto01 <augusto01@gmail.com>
2024-06-01 16:51:38 +0000

    Update submodule to 5.1

--- demo/06_TSP_SA/mainTSP-fcore-brkga.py
+++ demo/06_TSP_SA/mainTSP-fcore-brkga.py
@@ -1,56 +1,71 @@
-import time
+# OptFrame Python Demo TSP - Traveling Salesman Problem
+
+from typing import List
 import random
-from typing import List, Tuple, Union
+
 from optframe import *
 from optframe.protocols import *
-from optframe.components import Move, NSIterator
-from optframe.heuristics import BRKGA
-from optframe.core import LibArrayDouble
+
 
 class SolutionTSP(object):
     def __init__(self):
-        self.n: int = 0
-        self.cities: List[int] = []
+        # number of cities in solution
+        self.n : int = 0
+        # visited cities as a list
+        self.cities : List[int] = []
 
+    # MUST provide some printing mechanism
     def __str__(self):
         return f"SolutionTSP(n={self.n};cities={self.cities})"
-
+    
 class ProblemContextTSP(object):
     def __init__(self):
+        # float engine for OptFrame
         self.engine = Engine(APILevel.API1d)
+        # number of cities
         self.n = 0
+        # x coordinates
         self.vx = []
+        # y coordinates
         self.vy = []
+        # distance matrix
         self.dist = []
+        
+   # Example: "3\n1 10 10\n2 20 20\n3 30 30\n"
 
     def load(self, filename: str):
         with open(filename, 'r') as f:
             lines = f.readlines()
             self.n = int(lines[0])
             for i in range(self.n):
-                id_x_y = lines[i+1].split()
-                self.vx.append(float(id_x_y[1]))  # Allow floats
-                self.vy.append(float(id_x_y[2]))  # Allow floats
+               id_x_y = lines[i+1].split()
+               # ignore id_x_y[0]
+               self.vx.append(int(id_x_y[1]))
+               self.vy.append(int(id_x_y[2]))
+            #
             self.dist = [[0 for col in range(self.n)] for row in range(self.n)]
             for i in range(self.n):
-                for j in range(self.n):
-                    self.dist[i][j] = round(self.euclidean(self.vx[i], self.vy[i], self.vx[j], self.vy[j]))
+               for j in range(self.n):
+                  self.dist[i][j] = round(self.euclidean(self.vx[i], self.vy[i], self.vx[j], self.vy[j]))
 
     def euclidean(self, x1, y1, x2, y2):
         import math
-        return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+        return math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))
 
     def __str__(self):
         return f"ProblemContextTSP(n={self.n};vx={self.vx};vy={self.vy};dist={self.dist})"
-
+# continuation of ProblemContextTSP class...
     @staticmethod
     def minimize(pTSP: 'ProblemContextTSP', s: SolutionTSP) -> float:
+        assert (s.n == pTSP.n)
+        assert (len(s.cities) == s.n)
+        # remember this is an API1d method
         f = 0.0
         for i in range(pTSP.n-1):
-            f += pTSP.dist[s.cities[i]][s.cities[i + 1]]
-        f += pTSP.dist[s.cities[int(pTSP.n) - 1]][s.cities[0]]
+          f += pTSP.dist[s.cities[i]][s.cities[i + 1]];
+        f += pTSP.dist[s.cities[int(pTSP.n) - 1]][s.cities[0]];
         return f
-
+# continuation of ProblemContextTSP class...
     @staticmethod
     def generateSolution(problemCtx: 'ProblemContextTSP') -> SolutionTSP:
         sol = SolutionTSP()
@@ -60,117 +75,176 @@ class ProblemContextTSP(object):
         sol.n = problemCtx.n
         return sol
 
+# optional tests...
+assert isinstance(SolutionTSP, XSolution)            # composition tests 
+assert isinstance(ProblemContextTSP,  XProblem)      # composition tests 
+assert isinstance(ProblemContextTSP,  XConstructive) # composition tests    
+assert isinstance(ProblemContextTSP,  XMinimize)     # composition tests
+
+from optframe.components import Move
+
 class MoveSwapClass(Move):
     def __init__(self, _i: int = 0, _j: int = 0):
         self.i = _i
         self.j = _j
-
     def __str__(self):
         return "MoveSwapClass(i="+str(self.i)+";j="+str(self.j)+")"
-
     def apply(self, problemCtx, sol: SolutionTSP) -> 'MoveSwapClass':
         aux = sol.cities[self.j]
         sol.cities[self.j] = sol.cities[self.i]
         sol.cities[self.i] = aux
+        # must create reverse move (j,i)
         return MoveSwapClass(self.j, self.i)
-
     def canBeApplied(self, problemCtx, sol: SolutionTSP) -> bool:
         return True
-
     def eq(self, problemCtx, m2: 'MoveSwapClass') -> bool:
         return (self.i == m2.i) and (self.j == m2.j)
+
+assert isinstance(MoveSwapClass, XMove)       # composition tests
+assert MoveSwapClass in Move.__subclasses__() # classmethod style
+
+#from optframe.components import NS
 
 class NSSwap(object):
     @staticmethod
     def randomMove(pTSP, sol: SolutionTSP) -> MoveSwapClass:
+        import random
         n = sol.n
         i = random.randint(0, n - 1)
         j = i
-        while j <= i:
+        while  j <= i:
             i = random.randint(0, n - 1)
             j = random.randint(0, n - 1)
+        # return MoveSwap(i, j)
         return MoveSwapClass(i, j)
+    
+#assert NSSwap in NS.__subclasses__()   # optional test
+
+#from optframe.components import NSSeq
+from optframe.components import NSIterator
+
+# For NSSeq, one must provide a Move Iterator
+# A Move Iterator has five actions: Init, First, Next, IsDone and Current
 
 class IteratorSwap(NSIterator):
     def __init__(self, _i: int, _j: int):
         self.i = _i
         self.j = _j
-
     def first(self, pTSP: ProblemContextTSP):
         self.i = 0
         self.j = 1
-
     def next(self, pTSP: ProblemContextTSP):
         if self.j < pTSP.n - 1:
-            self.j = self.j + 1
+            self.j = self.j+1
         else:
             self.i = self.i + 1
             self.j = self.i + 1
-
     def isDone(self, pTSP: ProblemContextTSP):
         return self.i >= pTSP.n - 1
-
     def current(self, pTSP: ProblemContextTSP):
         return MoveSwapClass(self.i, self.j)
-
+    
+assert IteratorSwap in NSIterator.__subclasses__()   # optional test
+    
 class NSSeqSwap(object):
     @staticmethod
     def randomMove(pTSP: ProblemContextTSP, sol: SolutionTSP) -> MoveSwapClass:
-        return NSSwap.randomMove(pTSP, sol)
-
+        return NSSwap.randomMove(pTSP, sol)  # composition
+    
     @staticmethod
     def getIterator(pTSP: ProblemContextTSP, sol: SolutionTSP) -> IteratorSwap:
         return IteratorSwap(-1, -1)
 
+#assert NSSeqSwap in NSSeq.__subclasses__()   # optional test
+# =========================================
+# begins main() python script for TSP BRKGA
+# =========================================
+from optframe.heuristics import *
+
+from optframe.protocols import XConstructiveRK
+from optframe.core import LibArrayDouble
+
+#
+# random constructive: updates parameter ptr_array_double of type (LibArrayDouble*)
+#
 class RKConstructiveTSP(object):
     @staticmethod
-    def generateRK(problemCtx: ProblemContextTSP, ptr_array_double: LibArrayDouble) -> int:
+    def generateRK(problemCtx: ProblemContextTSP, ptr_array_double : LibArrayDouble) -> int:
         rkeys = []
         for i in range(problemCtx.n):
-            key = random.random()
+            key = random.random() # [0,1] uniform
             rkeys.append(key)
+        #
         ptr_array_double.contents.size = len(rkeys)
         ptr_array_double.contents.v = engine.callback_adapter_list_to_vecdouble(rkeys)
         return len(rkeys)
 
+
+from optframe.core import LibArrayDouble
+from typing import Tuple, Union
+
+import ctypes
+
+#
+# decoder function: receives a problem instance and an array of random keys (as LibArrayDouble)
+#
+
 class DecoderTSP(object):
     @staticmethod
-    def decodeSolution(pTSP: ProblemContextTSP, array_double: LibArrayDouble) -> SolutionTSP:
+    def decodeSolution(pTSP: ProblemContextTSP, array_double : LibArrayDouble) -> SolutionTSP:
+        #
         sol = SolutionTSP()
+        #
         lpairs = []
         for i in range(array_double.size):
             p = [array_double.v[i], i]
             lpairs.append(p)
+        #
+        #print("lpairs: ", lpairs)
         sorted_list = sorted(lpairs)
+        #print("sorted_list: ", sorted_list)
+        #
         sol.n = pTSP.n
         sol.cities = []
         for i in range(array_double.size):
-            sol.cities.append(sorted_list[i][1])
+            sol.cities.append(sorted_list[i][1]) # append index of city in order
         return sol
 
     @staticmethod
-    def decodeMinimize(pTSP: ProblemContextTSP, array_double: LibArrayDouble, needsSolution: bool) -> Tuple[Union[SolutionTSP, None], float]:
+    def decodeMinimize(pTSP: ProblemContextTSP, array_double : LibArrayDouble, needsSolution: bool) -> Tuple[Union[SolutionTSP,None], float]:
+        #
+        # print("decodeMinimize! needsSolution="+str(needsSolution), flush=True)
         sol = DecoderTSP.decodeSolution(pTSP, array_double)
+        #
+        # NOW WILL GET EVALUATION VALUE
         e = ProblemContextTSP.minimize(pTSP, sol)
+        # FINALLY, WILL RETURN WHAT IS REQUIRED
         if not needsSolution:
             return (None, e)
         else:
             return (sol, e)
 
-random.seed(0)
 
+
+
+# set random seed for system
+random.seed(0) # bad generator, just an example..
+
+# loads problem from filesystem
 pTSP = ProblemContextTSP()
-pTSP.load('instances/01_berlin52.tsp')
+pTSP.load('tsp-example.txt')
 
-print("problem=", pTSP)
+print("problem=",pTSP)
 
 import optframe
 print(str(optframe.__version__))
 pTSP.engine.welcome()
 
+
+# Register Basic Components
 comp_list = pTSP.engine.setup(pTSP)
 print(comp_list)
-
+#
 ev_idx = comp_list[1]
 print("evaluator id:", ev_idx)
 
@@ -188,42 +262,21 @@ print("drk_rk_id=", drk_rk_id)
 
 pTSP.engine.list_components("OptFrame:")
 
+#print("")
+#print("WILL CREATE DecoderRandomKeys FROM DecoderRandomKeysNoEvaluation!")
+#drk = DecoderRandomKeys(pTSP.engine, ev_idx, dec_rk_idx)
+#drk_rk_id = drk.get_id()
+#print("drk_rk_id=", drk_rk_id)
+
+# =======================
+# pTSP.engine.experimental_set_parameter("ENGINE_LOG_LEVEL", "4")
 print("")
 print("will start BRKGA for 3 seconds")
-
-# BRKGA parameters
-population_size = 30
-num_generations = 2000
-elite_proportion = 0.4
-mutant_proportion = 0.3
-elite_inheritance_probability = 0.6
-
-brkga = BRKGA(pTSP.engine, drk_rk_id, c_rk_idx, population_size, num_generations, elite_proportion, mutant_proportion, elite_inheritance_probability)
+brkga = BRKGA(pTSP.engine, drk_rk_id, c_rk_idx, 30, 1000, 0.4, 0.3, 0.6)
 
 pTSP.engine.list_components("OptFrame:")
 
-# Start timer
-start_time = time.time()
+lout = brkga.search(3.0)
+print("Best solution: ",   lout.best_s)
+print("Best evaluation: ", lout.best_e)
 
-# Initial solution and evaluation
-initial_sol = ProblemContextTSP.generateSolution(pTSP)
-initial_eval = ProblemContextTSP.minimize(pTSP, initial_sol)
-
-lout = brkga.search(3000.0)
-best_solution = lout.best_s
-best_evaluation = lout.best_e
-
-# Calculate improvement
-improvement = 100.0 * (initial_eval - best_evaluation) / initial_eval
-
-# Print results
-print("Initial evaluation:", initial_eval)
-print("Initial solution:", initial_sol.cities)
-print(f"BRKGA parameters: population size = {population_size}, num generations = {num_generations}, elite proportion = {elite_proportion}, mutant proportion = {mutant_proportion}, elite inheritance probability = {elite_inheritance_probability}")
-print("Best solution found by BRKGA: Evaluation function value =", best_evaluation)
-print("Best solution:", best_solution.cities)
-print("Improvement:", improvement, "%")
-
-# Print execution time
-execution_time = time.time() - start_time
-print("Execution Time:", execution_time, "seconds")
